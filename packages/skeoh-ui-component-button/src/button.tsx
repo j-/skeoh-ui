8050eda84f6fb469133afd1d431af83547467fd9
j- <j@skeoh.com>
2017-03-04 23:17:58 +1100

    Add classnames

--- packages/skeoh-ui-component-button/src/button.tsx
+++ packages/skeoh-ui-component-button/src/button.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as classNames from 'classnames';
 
 export interface IProps {
 	[prop: string]: any,
@@ -6,7 +7,7 @@ export interface IProps {
 
 const Button = (props: IProps) => (
 	<button
-		className="skeoh-ui-button"
+		className={ classNames("skeoh-ui-button", props.className) }
 		{ ...props }
 	/>
 );
