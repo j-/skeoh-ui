4b2c092be34b12192799656f12c226cf50ac20ed
j- <j@skeoh.com>
2017-03-05 17:06:45 +1100

    Extend button group class name

--- packages/skeoh-ui-component-button/src/group.tsx
+++ packages/skeoh-ui-component-button/src/group.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
+import * as classNames from 'classnames';
 
 export interface IProps extends React.Props<HTMLDivElement> {
-
+	[prop: string]: any,
 }
 
 const ButtonGroup = (props: IProps) => (
 	<div
-		className="skeoh-ui-button-group"
+		className={ classNames("skeoh-ui-button-group", props.className) }
 		{ ...props }
 	/>
 );
