b4fafa1d4fc5853d75435a26b387a4d17ec8a859
j- <j@skeoh.com>
2017-03-09 22:12:39 +1100

    Add submit button class

--- packages/skeoh-ui-component-button/src/submit.tsx
+++ packages/skeoh-ui-component-button/src/submit.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
+import * as classNames from 'classnames';
 import Button, { IProps as IButtonProps } from './button';
 
 export interface IProps extends IButtonProps {
 
 }
 
-const Submit = ({ children, ...props }: IProps) => (
-	<Button type="submit" { ...props }>
+const Submit = ({ children, className, ...props }: IProps) => (
+	<Button
+		type="submit"
+		className={ classNames('skeoh-ui-button-submit', className) }
+		{ ...props }
+	>
 		{ children || 'Submit' }
 	</Button>
 );
