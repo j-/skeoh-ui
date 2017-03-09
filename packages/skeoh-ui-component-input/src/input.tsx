02fa97f5e4c78d2be67e7d9639eba9ccf07c26f3
j- <j@skeoh.com>
2017-03-09 22:26:23 +1100

    Destructure input component props

--- packages/skeoh-ui-component-input/src/input.tsx
+++ packages/skeoh-ui-component-input/src/input.tsx
@@ -5,11 +5,11 @@ export interface IProps extends React.HTMLAttributes<HTMLInputElement> {
 
 }
 
-const Input = (props: IProps) => (
+const Input = ({ className, ...props }: IProps) => (
 	<input
 		type="text"
 		{ ...props }
-		className={ classNames('skeoh-ui-input', props.className) }
+		className={ classNames('skeoh-ui-input', className) }
 	/>
 );
 
