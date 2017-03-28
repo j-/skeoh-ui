90137fc1a46842765c4f25be499db4a8fd8870ac
j- <j@skeoh.com>
2017-03-28 12:16:55 +1100

    Remove `/src` from import paths
    This was added to assist VSCode in finding typings for each module
    without needing to rebuild dependencies. As it turns out, Code is smart
    enough to pull typings from the source without the need to rebuild.
    
    By updating these paths we can run each module as 100% JavaScript when
    built -- TypeScript is just used to compile, not to run.

--- packages/guide/src/components/Demo.tsx
+++ packages/guide/src/components/Demo.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as CodeMirror from 'react-codemirror';
-import { Submit } from '@skeoh-ui/component-button/src';
-import { FillHorizontal } from '@skeoh-ui/component-fill/src';
+import { Submit } from '@skeoh-ui/component-button';
+import { FillHorizontal } from '@skeoh-ui/component-fill';
 import * as ts from 'typescript';
 import bootstrap from '../safe-imports';
 
